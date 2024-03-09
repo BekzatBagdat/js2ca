16ac083f255cfd0b47fb4cfce0007107a256b72d
Bekzat Bagdat <89454659+BekzatBagdat@users.noreply.github.com>
2024-03-09 02:44:27 +0100

    updt

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,6 @@ import { runUserReg } from "./modules/signup.js";
 import { runUserLogin } from "./modules/login.js";
 import { setProfileData } from "./modules/profile.js";
 
-/*Register and Login User*/
 runUserReg();
 runUserLogin();
-/*set username to the profile page*/
 setProfileData();
